7fbdb79e74e282cc7794c821ba14648364d09dd1
shernshiou <shernshiou@gmail.com>
2017-05-03 13:34:59 +0200

    Data assoc

--- Term2/03-CarND-Kidnapped-Vehicle-Project/src/particle_filter.h
+++ Term2/03-CarND-Kidnapped-Vehicle-Project/src/particle_filter.h
@@ -72,7 +72,7 @@ public:
 	 * @param predicted Vector of predicted landmark observations
 	 * @param observations Vector of landmark observations
 	 */
-	void dataAssociation(std::vector<LandmarkObs> predicted, std::vector<LandmarkObs>& observations);
+	void dataAssociation(std::vector<LandmarkObs> predictions, std::vector<LandmarkObs>& observations);
 
 	/**
 	 * updateWeights Updates the weights for each particle based on the likelihood of the

5220912ee274fa315cc35a6113295f4abde85391
shernshiou <shernshiou@gmail.com>
2017-05-18 12:43:27 +0200

    PID Class

--- Term2/04-CarND-Controls-PID/src/PID.cpp
+++ Term2/04-CarND-Controls-PID/src/PID.cpp
@@ -6,16 +6,26 @@ using namespace std;
 * TODO: Complete the PID class.
 */
 
-PID::PID() {}
+PID::PID() {
+  this->p_error = 0.0;
+  this->i_error = 0.0;
+  this->d_error = 0.0;
+}
 
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+  this->Kp = Kp;
+  this->Ki = Ki;
+  this->Kd = Kd;
 }
 
 void PID::UpdateError(double cte) {
+  this->i_error += cte;
+  this->d_error = cte - p_error;
+  this->p_error = cte;
 }
 
 double PID::TotalError() {
+  return -this->Kp * this->p_error - this->Ki * this->i_error - this->Kd * this->d_error;
 }
-

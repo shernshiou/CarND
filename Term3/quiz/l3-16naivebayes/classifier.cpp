9a688005437aea0d430ed8472463b17089a0bace
shernshiou <shernshiou@gmail.com>
2017-07-19 11:30:49 +0200

    Clang makefile

--- Term3/quiz/l3-16naivebayes/classifier.cpp
+++ Term3/quiz/l3-16naivebayes/classifier.cpp
@@ -1,88 +1,59 @@
-#include "classifier.h"
 #include <iostream>
+#include <sstream>
 #include <fstream>
 #include <math.h>
 #include <vector>
+#include "classifier.h"
 
-using namespace std;
-
-vector<vector<double> > Load_State(string file_name)
-{
-    ifstream in_state_(file_name.c_str(), ifstream::in);
-    vector< vector<double >> state_out;
-    string line;
-
-    while (getline(in_state_, line))
-    {
-
-    	istringstream iss(line);
-    	vector<double> x_coord;
-    	double state1;
-	    double state2;
-	    double state3;
-	    double state4;
-	    iss >> state1;
-	    x_coord.push_back(state1);
-	    iss >> state2;
-	    x_coord.push_back(state2);
-    	iss >> state3;
-    	x_coord.push_back(state3);
-	    iss >> state4;
-	    x_coord.push_back(state4);
-
-	    state_out.push_back(x_coord);
-    }
-    return state_out;
-}
-vector<string> Load_Label(string file_name)
-{
-    ifstream in_label_(file_name.c_str(), ifstream::in);
-    vector< string > label_out;
-    string line;
-    while (getline(in_label_, line))
-    {
-    	istringstream iss(line);
-    	string label;
-	    iss >> label;
-
-	    label_out.push_back(label);
-    }
-    return label_out;
+/**
+ * Initializes GNB
+ */
+GNB::GNB() {
 
 }
 
-int main() {
+GNB::~GNB() {}
 
-    vector< vector<double> > X_train = Load_State("./train_states.txt");
-    vector< vector<double> > X_test  = Load_State("./test_states.txt");
-    vector< string > Y_train  = Load_Label("./train_labels.txt");
-    vector< string > Y_test   = Load_Label("./test_labels.txt");
+void GNB::train(vector<vector<double>> data, vector<string> labels)
+{
 
-	cout << "X_train number of elements " << X_train.size() << endl;
-	cout << "X_train element size " << X_train[0].size() << endl;
-	cout << "Y_train number of elements " << Y_train.size() << endl;
+	/*
+		Trains the classifier with N data points and labels.
 
-	GNB gnb = GNB();
+		INPUTS
+		data - array of N observations
+		  - Each observation is a tuple with 4 values: s, d,
+		    s_dot and d_dot.
+		  - Example : [
+			  	[3.5, 0.1, 5.9, -0.02],
+			  	[8.0, -0.3, 3.0, 2.2],
+			  	...
+		  	]
 
-	gnb.train(X_train, Y_train);
+		labels - array of N labels
+		  - Each label is one of "left", "keep", or "right".
+	*/
+}
 
-	cout << "X_test number of elements " << X_test.size() << endl;
-	cout << "X_test element size " << X_test[0].size() << endl;
-	cout << "Y_test number of elements " << Y_test.size() << endl;
+string GNB::predict(vector<double>)
+{
+	/*
+		Once trained, this method is called and expected to return
+		a predicted behavior for the given observation.
 
-	int score = 0;
-	for(int i = 0; i < X_test.size(); i++)
-	{
-		vector<double> coords = X_test[i];
-		string predicted = gnb.predict(coords);
-		if(predicted.compare(Y_test[i]) == 0)
-		{
-			score += 1;
-		}
-	}
+		INPUTS
 
-	float fraction_correct = float(score) / Y_test.size();
-	cout << "You got " << (100*fraction_correct) << " correct" << endl;
+		observation - a 4 tuple with s, d, s_dot, d_dot.
+		  - Example: [3.5, 0.1, 8.5, -0.2]
 
-	return 0;
+		OUTPUT
+
+		A label representing the best guess of the classifier. Can
+		be one of "left", "keep" or "right".
+		"""
+		# TODO - complete this
+	*/
+
+	return this->possible_labels[1];
+
 }
